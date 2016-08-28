9630eb88af3bacaca9de02b587cbfda5c2f97ec4
Tom Mowad <tmowad@gmail.com>
2016-08-28 16:17:38 -0700

    Implementation of Deck.

---
+++ WarDeck.java
@@ -0,0 +1,13 @@
+public class WarDeck implements Deck {
+  public void create( int numberOfSuits, int numberOfRanks ) {
+    throws new RuntimeException("not implemented");
+  }
+
+  public void shuffle() {
+    throws new RuntimeException("not implemented");
+  }
+
+  public Card deal() {
+    throws new RuntimeException("not implemented");
+  }
+}
