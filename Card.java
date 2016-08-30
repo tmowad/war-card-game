108fadb19bc5193213174126a769e27191a2d215
Tom Mowad <tmowad@gmail.com>
2016-08-30 00:41:54 -0700

    Clean up commenting and a bit of code cleanup on Card class.

--- Card.java
+++ Card.java
@@ -1,9 +1,9 @@
 /**
  * Representation of a card.  
  * 
- * We could represent rank and suit as simple ints, but since different card 
- * games may have different semantics, we can try and rep these fields as 
- * interfaces that are implemented per-game.
+ * We could represent rank and suit as simple ints or enum types, but since 
+ * different card games have different semantics, we can represent these fields
+ * as interfaces with per-game implementations.
  *
  * We will figure out the details of what these interfaces need to do as we
  * solve this problem.  (Ideally you can decide the interface up front, but
@@ -14,23 +14,48 @@ public class Card {
     private Rank rank;
     private Suit suit;
 
+    /**
+     * Constructor, a Card takes a suit and a rank.
+     * @param rank The rank of a card.
+     * @param suit The suit of a card.  
+     */
     public Card(Rank rank, Suit suit) {
         this.rank = rank;
         this.suit = suit;
     }
 
+    /**
+     * We define two cards as equal if their rank and suit are equal.  
+     * @param other An object we are testing equality with.
+     * @return true if other is equal to this, false otherwise.  
+     */
     public boolean equals(Object other) {
+        if (!(other instanceof Card)) {
+            return false;
+        }
         Card otherCard = (Card) other;
         return this.rank.equals(otherCard.rank) && this.suit.equals(otherCard.suit);
     }
 
+    /**
+     * Prints a simple string representation of this card.
+     * @return A string representation of this card.  
+     */
     public String toString() {
         return "Card<" + rank.toString() + " of " + suit.toString() + ">";
     }
 
     /**
-     * NOTE: Although compareTo is defined in the Comparable interface, we'll
-     * not implement that interface until/unless we see an actual need for it.
+     * Compares this card to another card.  
+     * @param otherCard The card being compared to.  
+     * @return -1 if this card is weaker than otherCard,
+     *          0 if this card is equal strength to otherCard,
+     *          1 if this card is stronger than otherCard. 
+     *
+     * NOTE: If I were to continue working on this, I'd consider moving 
+     * comparison out of the Card object, since a Card representation is
+     * fairly independent of the game being played, but the comparison
+     * function is definitely dependent on the game being played.  
      */
     public int compareTo(Card otherCard) {
         return this.rank.compareTo(otherCard.rank);
