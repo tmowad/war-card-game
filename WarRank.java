29ceca455536855c272f2cd29a391c0f71150f81
Tom Mowad <tmowad@gmail.com>
2016-08-30 01:57:55 -0700

    Add basic javadoc comments to a few classes.

--- WarRank.java
+++ WarRank.java
@@ -1,32 +1,55 @@
+/**
+ * Implementation of Rank interface suitable for the game of war.
+ */
 class WarRank implements Rank {
     private int rankValue;
 
+    /**
+     * Constructor.
+     * @param rank The rank for this WarRank.
+     */
     WarRank(int rank) {
         this.rankValue = rank;
     }
 
+    /**
+     * Returns the integer value of this rank.
+     * @return The integer value of this rank.
+     */
     public int getValue() {
         return rankValue;
     }
 
+    /**
+     * Tests equality with another object.
+     * @param other The other object with which to test equality.
+     * @return true if the other object is a Rank and has equal value to this,
+     *         false otherwise.
+     */
     public boolean equals(Object other) {
+        if (!(other instanceof Rank)) {
+            return false;
+        }
         Rank otherRank = (Rank) other;
         return rankValue == otherRank.getValue();
     }
 
+    /**
+     * Returns a string representation of this rank.
+     * @return A string representation of this rank.
+     */
     public String toString() {
         return "" + rankValue;
     }
 
     /**
-     * NOTE: In the real game of War, the standard 52-card deck is used, and 
-     * Aces are considered high.  Since our version can customize the number of
-     * rank values, we will consider the Rank values as natural numbers, 1, 2,
-     * ... to [numRankValues], with no concept of an Ace being value 1.  
-     *
-     * If we wanted, we could provide an implementation of Rank interface 
-     * that implemented that standard 52-card deck, where we could take into 
-     * account whether or not either card had a '1' value.  
+     * Compare this rank to another rank in terms of the game of war.  
+     * 
+     * Since we allow any integer number of ranks, it doesn't necessarily make
+     * sense to use the "Ace is high" semantics since, what if we have 3 rank
+     * values?  Or 25 rank values?  Granted, we could start at 1 and always
+     * say 1 beats all, but since its not that interesting, I'll just allow the
+     * deck to start with 0 and have no special meaning.  
      */
     public int compareTo(Rank other) {
         if (rankValue == other.getValue()) {
--- WarSuit.java
+++ WarSuit.java
@@ -1,17 +1,45 @@
+/**
+ * Implementation of Suit interface suitable for the game of war.
+ */
 class WarSuit implements Suit {
+    /**
+     * Name of the suit.  
+     */
     private String suitName;
 
+    /**
+     * Constructor. 
+     * @param name The name of this suit.  
+     */
     WarSuit(String name) {
         this.suitName = name;
     }
 
+    /**
+     * Returns the name of this suit.
+     * @return The name of this suit. 
+     */
     public String getName() {
         return suitName;
     }
+
+    /**
+     * Tests equality of this suit with another object.
+     * @param o The other object to test equality against.
+     * @return true if o is a Suit and its name equals this suit's name.
+     */
     public boolean equals(Object o) {
+        if (!(o instanceof Suit)) {
+            return false;
+        }
         Suit otherSuit = (Suit) o;
         return suitName.equals(otherSuit.getName());
     }
+
+    /**
+     * Returns a string representation of this suit.
+     * @return A string representation of this suit.
+     */
     public String toString() {
         return suitName;
     }
